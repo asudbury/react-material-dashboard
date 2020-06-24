cb921df9545a78faa828b1801dcbf49c78488e5f
Adrian Sudbury <sudburya@gmail.com>
2020-06-24 10:20:54 +0100

    Tidy up of Source Code module

--- src/views/SourceCode/SourceCode.js
+++ src/views/SourceCode/SourceCode.js
@@ -2,13 +2,12 @@
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import Hidden from "@material-ui/core/Hidden";
+
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
-import CardBody from "components/Card/CardBody.js";
 import Button from '@material-ui/core/Button';
 
 import styles from "assets/jss/material-dashboard-react/views/iconsStyle.js";
