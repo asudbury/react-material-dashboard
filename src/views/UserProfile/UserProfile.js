4588348f68e5282e1639404a507340d26244ba50
Adrian Sudbury <sudburya@gmail.com>
2021-06-15 07:37:43 +0100

    Fixed issue.

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -13,8 +13,6 @@ import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import avatar from "assets/img/faces/marc.jpg";
-
 const styles = {
   cardCategoryWhite: {
     color: "rgba(255,255,255,.62)",
